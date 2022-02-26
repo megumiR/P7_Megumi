d85a7a8613aad2013bfa332ca8fa9082470a4e24
megumiR <megshono76@gmail.com>
2022-02-26 16:08:11 +0100

    creer appjs et server js pour backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 /************ importer framework , modules **********/
+//pour mongodb et mongoose
+const mongoose = require('mongoose');
 const express = require('express');
 
 /************ FIN : importer framework , modules ************/
 
 
-/************  ******************/
+/*  Enleve apres --MongoDB Atlas         **/
+mongoose.connect('mongodb+srv://megumi:JZw7qlKVtgp24sVW@clusterprojet6piiquante.ybmmt.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+//pw: JZw7qlKVtgp24sVW
+/*************************/
 
 
 //Appeler la method express(qui permet de creer application express)
@@ -12,4 +21,4 @@ const app = express();
 //acceder aux requetes json.body
 app.use(express.json());
 
-console.log(app);
+
