55907b930ee1acd57e5ffa4a7d0b395853c525c6
megumiR <megshono76@gmail.com>
2022-03-25 15:33:47 +0100

    la requete post pour creer un post: sans image, form-data n Json sur postman OK

--- backend/controlers/post.js
+++ backend/controlers/post.js
@@ -1,19 +1,18 @@
 /********* importer des fichier , modules *************/
-//const bcrypt = require('bcrypt');
 const connection = require('../db__connection');  //importer l'info de mysql 
-const multer = require('../middleware/multer-config');
-const fs = require('fs');
-//const jwt = require('jsonwebtoken');
 
-/********* la logique *********/
-//post un post  ?????????????????????? S'il n'y a pas de "file"
+const fs = require('fs');
+/********* FIN: importer des fichier , modules *************/
+
+
+/***************** Creer un post  ***************/
+//////////////////post un post  ?? S'il n'y a pas de "file"
 exports.createPost = /*async(req, res, next) => {
     console.log('post one article---------');
     console.log(req.body);
-    let userId = req.body.utilisateur_id;  //the same with :parseInt(req.body.utilisateur_id)
     // only JSON file works. not the form-data on postman
     let sql = `INSERT INTO post (postname, comment, utilisateur_id) VALUES 
-      ('${ req.body.postname }', '${ req.body.comment }', '${userId}' )`; //sessionid?
+      ('${ req.body.postname }', '${ req.body.comment }', '${req.headers.utilisateur_id}' )`; //sessionid?
     await connection.query( sql, (err, result) => {
       if (err) {
         throw err;
@@ -23,27 +22,47 @@ exports.createPost = /*async(req, res, next) => {
       res.status(201).json({ message: 'Votre post est bien crée!'});
     });
 };*/
+/////////// s'il y a un fichier {oui traiter l'image}:{non traiter que l'objet}
 async(req, res, next) => {
-  console.log('post one article---------');
- // const postObject = JSON.parse(req.body.post);
-  const post = req.file ?              // s'il y a un fichier {oui traiter l'image}:{non traiter l'objet}
+  console.log('post one article---------');   // this works with the form-data n JSON on postman
+  const post = req.file ?              
   {
       ...JSON.parse(req.body.post),
-      image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+      image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`   
   } : { ...req.body };
-  console.log('post: ' + post);
-  let sql = `INSERT INTO post (postname, comment, image, utilisateur_id) VALUES 
-    ('${ req.body.postname }', '${ req.body.comment }', '${req.protocol}://${req.get('host')}/images/${req.file.filename}', '${req.body.utilisateur_id}')`; //${ req.body.image }
-  await connection.query( sql, (err, result) => {
+  console.log('post: ');
+  console.log(post);            
+  console.log(req.body.image);
+  console.log(req.headers.utilisateur_id);
+
+  let sqlWithoutImage = `INSERT INTO post (postname, comment, utilisateur_id ) VALUES 
+    ('${ req.body.postname }', '${ req.body.comment }', '${req.headers.utilisateur_id}')`;   ///need to put utilisateur_id into headers
+  await connection.query( sqlWithoutImage, (err, result) => {
     if (err) {
       throw err;
     }
-    console.log('L\'article post est inseré : ' + req.body.comment);
-    res.status(201).json({ message: 'Votre post est bien crée!'});
+    console.log('L\'article post(comment, postname) est inseré : ' + req.body.comment);
+    res.status(201).json({ message: 'Votre post(sans image) est bien crée!'});
   });
-  res.status(400).json({ error });             
+  if (req.body.image) {
+    let sqlWithImage = `INSERT INTO post (postname, comment, image, utilisateur_id ) VALUES (
+    '${ req.body.postname }', '${ req.body.comment }', 
+    ('LOAD_FILE('${req.protocol}://${req.get('host')}/images/${req.file.filename}')'
+    '${req.headers.utilisateur_id}')`; 
+    connection.query( sqlWithImage, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    console.log('L\'article post(avec image) est inseré : ' + req.body.image);
+    res.status(201).json({ message: 'Votre post(avec image) est bien crée!'});
+    });
+  }       
 };
-exports.showallposts = async(req, res, next) => {
+/***************** FIN: Creer un post  ***************/
+
+
+/******************* Afficher tous les posts *********************/
+exports.showallposts = async(req, res, next) => {     ///It works with images too?
     console.log('home with posts---------');
     let sql = `SELECT * FROM post`;
     await connection.query( sql, (err, result) => {
@@ -58,6 +77,10 @@ exports.showallposts = async(req, res, next) => {
       }
     });
 };
+/******************* FIN: Afficher tous les posts *********************/
+
+
+/************ Liker/ Disliker un post ******************/
 //  IL FAUT TESTER depuis la
 exports.liker = async(req, res, next) => {
   console.log('Liker un post---------');
@@ -67,11 +90,12 @@ exports.liker = async(req, res, next) => {
       throw err;
     }
     console.log('specifié post por liker');
-
   });
-
 };
-//modifier  image?
+/************ FIN: Liker/ Disliker un post ******************/
+
+
+/************* Modifier un post **************/
 exports.updatePost = async(req, res, next) => {
     console.log('update specific post---------');  
     let sqlFind = `SELECT * FROM post WHERE id = ${ req.params.id }`;
@@ -96,7 +120,10 @@ exports.updatePost = async(req, res, next) => {
       res.status(201).json({ message: 'modifié' });
     });
 };
-//supprimer
+/************* FIN: Modifier un post **************/
+
+
+/************* Supprimer un post ******************/
 exports.deletePost = async(req, res, next) => {
   console.log('delete specific post---------');
   let sqlFind = `SELECT * FROM post WHERE id = ${ req.params.id }`;
@@ -118,6 +145,9 @@ exports.deletePost = async(req, res, next) => {
   });
   
 };
+/************* FIN: Supprimer un post ******************/
+
+
 /** session id?????????????????????//
 const cookieParser = require('cookie-parser');
 const session = require('express-session')
