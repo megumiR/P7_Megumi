d1aeaacff6f67b56cc9c2d25342fb2cb69924e7d
megumiR <megshono76@gmail.com>
2022-05-10 19:09:42 +0200

    probleme: formData perdu sur backend pour Liker

--- backend/controlers/like.js
+++ backend/controlers/like.js
@@ -6,47 +6,47 @@ const connection = require("../db__connection"); //importer l'info de mysql
 /************ Liker/ Disliker un post ******************/
 exports.liker = async (req, res, next) => {
     console.log("Liker un post---------");
-    console.log(req.body);
-    //console.log("req body" + req.body);
+    console.log(req.userId);
+    console.log("req body likes?" + req.body.likes);
    // console.log(JSON.stringify(req.body));
       // si l'user a deja Liké ce post et enregistré sur DB
-      let sqlFind = `SELECT * FROM post_likes WHERE post_id = '${req.params.id}' AND user_id = '${req.body.user_id}'`;
-      connection.query(sqlFind, (err, res) => {
+    /*  let sqlFind = `SELECT * FROM post_likes WHERE post_id = '${req.params.id}' AND user_id = '${req.userId}'`;
+      await connection.query(sqlFind, (err, res) => {
         if (err) {
           return res.status(400).json({ message: "erreur : savoir si c'est déjà Liker" });
         }
         console.log("step1:Already reacted to this post?");
-        console.log(res);
-        let sqlLike = `INSERT INTO post_likes (post_id, user_id, likes) VALUES ('${req.params.id}', '${req.body.user_id}', '${req.body.likes}')`;
-        connection.query(sqlLike, (err, result) => {
+      */  
+        let sqlLike = `INSERT INTO post_likes (post_id, user_id, likes) VALUES ('${req.params.id}', '${req.userId}', '${req.body.likes}')`;
+        await connection.query(sqlLike, (err, result) => {
             if (err) {
               return res.status(400).json({ message: "erreur : Liker pour la 1ere fois" });
             }
             console.log("Liker/ Disliker post pour la 1ere fois");
             res.status(200).json({ message: "Le Like/Dislike est enregistré." });
         });
-      });
+     // });
   };
   /************ FIN: Liker/ Disliker un post ******************/
   /************ Enlever Liker/ Disliker  ******************/
   exports.removeLiker = async (req, res, next) => {
     console.log("remove Liker ---------");
       // si l'user a deja Liké ce post et enregistré sur DB
-      let sqlFind = `SELECT * FROM post_likes WHERE post_id = '${req.params.id}' AND user_id = '${req.body.user_id}'`;
+  /*    let sqlFind = `SELECT * FROM post_likes WHERE post_id = '${req.params.id}' AND user_id = '${req.userId}'`;
       connection.query(sqlFind, (err, res) => {
         if (err) {
           return res.status(400).json({ message: "erreur : il n'y a pas de post" });
         }
-        // delete the existed result
+  */      // delete the existed result
         let sqlRemoveLike = `DELETE FROM post_likes WHERE post_id = '${req.params.id}' AND user_id = '${req.body.user_id}'`;
-        connection.query(sqlRemoveLike, (err, result) => {
+        await connection.query(sqlRemoveLike, (err, result) => {
             if (err) {
             return res.status(400).json({ message: "erreur : supprimer le Liker" });
             }
             console.log("Liker/ Disliker supprimé");
             res.status(200).json({ message: "Le Like/Dislike est supprimé." });
         });
-      });  
+ //     });  
   };
   /************ FIN: Liker/ Disliker un post ******************/
   
