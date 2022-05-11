37a06b1652a559985d37ba8d836b16c53622bd25
megumiR <megshono76@gmail.com>
2022-05-11 19:43:03 +0200

    ajouter mailvalidator sur utilisateur.js/route et installer email-validator

--- backend/routes/utilisateur.js
+++ backend/routes/utilisateur.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();//importer middleware pour la validation de mot de passe
 const passvalidator = require('../middleware/passvalidator');
+const mailvalidator = require('../middleware/mailvalidator');
 
 const utilisateurCtrl = require('../controlers/utilisateur');
 
-router.post('/signup', passvalidator, utilisateurCtrl.signup);
-router.post('/login', utilisateurCtrl.login);
+router.post('/signup', passvalidator, mailvalidator, utilisateurCtrl.signup);
+router.post('/login', mailvalidator, utilisateurCtrl.login);
 
 module.exports = router;
