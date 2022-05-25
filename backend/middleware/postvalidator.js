b25fa5b8641153bf735fcf0266df56837b635327
megumiR <megshono76@gmail.com>
2022-05-25 16:19:55 +0200

    probleme: express-validator (back) ne marche pas

--- backend/middleware/postvalidator.js
+++ backend/middleware/postvalidator.js
@@ -1,4 +1,19 @@
 //importer le package de npm
+const { body, validationResult } = require("express-validator"); //import validator from 'validator';
+//
+module.exports = (req, res, next) => {
+  body(req.body.title).isLength({ min: 1 }) && body(req.body.content).isLength({ min: 1 } && body(req.body.content).isLength({ max: 150 }), (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+    //    return res.status(400).json({ error: "Le title ou/et la discription n'est pas valide" });
+        }
+        next();   
+    }
+  };
+
+/***
+ //importer le package de npm
 const validator = require("validator"); //import validator from 'validator';
 
 module.exports = (req, res, next) => {
@@ -19,3 +34,4 @@ module.exports = (req, res, next) => {
     // console.log({ error: "Le mot de passe n'est pas assez fort : majuscule, minuscule, chiffres, pas de l'espace, plus que 8 caractaires" });
   }
 };
+ */
